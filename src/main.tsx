9a8b98a588655e3b54ca49e109face32ef44b509
_ñ <knaka0209@gmail.com>
2024-01-22 19:35:18 +0900

    up, 0122a5

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 
 /*
 function App(){
   return(
-    <div>
-      <h1>hello</h1>
-    </div>
+  <div className="App">app
+  </div>
   )
 }
 */
-console.log("#client.ts");
-//root
+//
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-);
+)
+/**/
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,7 +5,8 @@ export function Csr() {
     return (
     <html>
         <head>
-        <title>welcome</title>
+            <title>welcome</title>
+            <link href="/public/static/style.css" rel="stylesheet" /> 
         </head>
         <div id="root"></div>
         <script type="module" src="/public/static/main.js"></script>
