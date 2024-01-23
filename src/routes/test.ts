91b2281a0d4f29af36752c8883b132e23a99861b
_ñ <knaka0209@gmail.com>
2024-01-24 08:25:45 +0900

    up, 0124a6

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -5,12 +5,9 @@ require('dotenv').config();
 import LibPg from '../lib/LibPg';
 
 
-/**
-* 
-* @param
-*
-* @return
-*/ 
+/*****************************
+todos -index
+******************************/
 router.post('/test1', async function(req: any, res: any) {
   try {
 console.log(req.body);
@@ -25,33 +22,6 @@ console.log(req.body);
     client.end();
 //      console.log(res.rows);
     res.json({ret: "OK", data: resulete.rows});
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-});
-/**
-* 
-* @param
-*
-* @return
-*/ 
-router.post('/create', async function(req: any, res: any) {
-  try {
-//console.log(body.userId);
-console.log(req.body);
-      const body = req.body;
-      const text = `
-      INSERT INTO public."Todo" (title, content, complete,
-      "userId", "createdAt", "updatedAt") 
-      VALUES($1, $2, 0, $3, current_timestamp, current_timestamp) RETURNING *
-      `;
-      const values = [body.title, body.content, body.userId ]
-      const client = LibPg.getClient();
-      const result = await client.query(text, values);
-      client.end();
-//console.log(result.rows);
-      res.json({ret: "OK", data: result.rows});
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
