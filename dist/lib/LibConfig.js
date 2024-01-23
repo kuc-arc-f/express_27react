52555e589cb46dd48877ba9e3c9afa0c32a7e600
_ñ <knaka0209@gmail.com>
2024-01-24 08:14:15 +0900

    up, 0124a4

--- dist/lib/LibConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var LibConfig = {
-    OK_CODE: 'OK',
-    NG_CODE: 'NG',
-};
-exports.default = LibConfig;
--- dist/pages/App.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Csr = void 0;
-var jsx_runtime_1 = require("react/jsx-runtime");
-console.log("env=", process.env.NODE_ENV);
-function Csr() {
-    return ((0, jsx_runtime_1.jsxs)("html", { children: [(0, jsx_runtime_1.jsxs)("head", { children: [(0, jsx_runtime_1.jsx)("title", { children: "welcome" }), (process.env.NODE_ENV === "develop") ? ((0, jsx_runtime_1.jsx)("link", { href: "/static/style.css", rel: "stylesheet" })) : ((0, jsx_runtime_1.jsx)("link", { href: "/public/static/style.css", rel: "stylesheet" }))] }), (0, jsx_runtime_1.jsx)("div", { id: "root" }), (process.env.NODE_ENV === "develop") ? ((0, jsx_runtime_1.jsx)("script", { type: "module", src: "/static/main.js" })) : ((0, jsx_runtime_1.jsx)("script", { type: "module", src: "/public/static/main.js" }))] }));
-}
-exports.Csr = Csr;
